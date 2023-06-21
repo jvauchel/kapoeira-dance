bafe0074fcb376ef426bd2c181f1f98fb2220996
Mehdi Rebiai <m.rebiai@lectra.com>
2023-06-21 13:58:17 +0200

    doc: complete abstract & init ci

---
+++ build-slides.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor \
+  asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
+  -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
+  -a revealjs_slideNumber=true \
+  -D public -o index.html \
+  presentation_fr.adoc
+
+#-a customcss=custom.css \
+
+
+# CONFERENCE_NAME="xxx"
+# CONFERENCE_PNG_BASE64=$(cat images/logo-${CONFERENCE_NAME}.png | base64 -w0) QRCODE_PNG_BASE64=$(cat images/qrcode.png | base64 -w0) \
+#   envsubst < custom.css > public/custom.css 
+
+touch public/.nojekyll
