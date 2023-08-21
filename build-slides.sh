bb1d05185d82d08927b7a2b09149371ab864068a
Johanna Vauchel <vauchel.johanna@gmail.com>
2023-08-21 17:07:45 +0200

    update slides

--- build-slides.sh
+++ build-slides.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 rm -rf public
+mkdir -p public
+
+
+CONFERENCE_PNG_BASE64=$(cat images/logo-webinar.png | base64 -w0) \
+QRCODE_PNG_BASE64=$(cat images/qrcode.png | base64 -w0) \
+  envsubst < custom.css > public/custom.css
 
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor:1.49.0 \
-  asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
+  asciidoctor-revealjs -a data-uri -a revealjs_theme=simple \
   -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
-  -a revealjs_slideNumber=true \
+  -a customcss=custom.css -a revealjs_slideNumber=true \
   -D public -o index.html \
   presentation_fr.adoc
 
