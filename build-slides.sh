631e9eb81cf46eeab6c3dbfb70070efb133cc0bf
Johanna Vauchel <vauchel.johanna@gmail.com>
2023-08-21 14:39:20 +0200

    delete public folder

--- build-slides.sh
+++ build-slides.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf public
+
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/documents asciidoctor/docker-asciidoctor:1.49.0 \
   asciidoctor-revealjs -a data-uri -a revealjs_theme=white \
   -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.9.2 -a revealjs_transition=fade \
