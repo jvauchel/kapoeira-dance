8276ea44a632b9b7c98a41ad6defcdb0b380e589
Mehdi Rebiai <m.rebiai@lectra.com>
2023-11-07 16:29:29 +0100

    feat: add capitole du libre

--- build-slides.sh
+++ build-slides.sh
@@ -2,7 +2,7 @@
 
 ASCIIDOCTOR_DOCKER_IMAGE="asciidoctor/docker-asciidoctor:1.55.0"
 REVEALJS_DIR="https://cdn.jsdelivr.net/npm/reveal.js@4.1.2"
-CONFERENCES=("webinar" "bdxio")
+CONFERENCES=("webinar" "bdxio" "capitoledulibre")
 
 cat summary.adoc > index.adoc
 
@@ -12,11 +12,11 @@ mkdir -p public
 for conf in "${CONFERENCES[@]}"
 do
   CONFERENCE_PNG_BASE64=$(cat images/logo-${conf}.png | base64 -w0) \
-  QRCODE_PNG_BASE64=$(cat images/qrcode.png | base64 -w0) \
+  QRCODE_PNG_BASE64=$(cat images/qrcode-slides.png | base64 -w0) \
     envsubst < custom.css > public/custom-${conf}.css
   docker run --name $(uuidgen) --rm -u $(id -u):$(id -g) -v $(pwd):/documents ${ASCIIDOCTOR_DOCKER_IMAGE} \
     asciidoctor-revealjs -a data-uri -a revealjs_theme=simple \
-    -a conf-${conf} \
+    -a conf-${conf} -a confname=${conf} \
     -a revealjsdir=${REVEALJS_DIR} -a revealjs_transition=fade \
     -a customcss=custom-${conf}.css -a revealjs_slideNumber=true \
     -D public -o index-${conf}.html \
